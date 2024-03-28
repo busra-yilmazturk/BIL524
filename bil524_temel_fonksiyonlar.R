7ba63bbc87db9a5d4eaf3b52922658e67dd92b2a
Busra Yilmazturk <busraa.tull@gmail.com>
2024-03-28 11:43:42 +0000

    csv loaded

--- bil524_temel_fonksiyonlar.R
+++ bil524_temel_fonksiyonlar.R
@@ -1,5 +1,10 @@
 # temel fonksiyonlar
 
-veri = read.csv("http://johnmuschelli.com/intro_to_r/data/Youth_Tobacco_Survey_YTS_Data.csv")
+# veriyi kaynagindan indirdikten sonra kendi
+# calisma alanimda /data isimli bir klasor acarak
+# icine yukledim
+veri = read.csv("data/Youth_Tobacco_Survey_YTS_Data.csv")
 View(veri)
 
+paste("Satır Sayısı: ", nrow(veri), sep=" ")
+paste("Kolon Sayısı: ", ncol(veri), sep=" ")
