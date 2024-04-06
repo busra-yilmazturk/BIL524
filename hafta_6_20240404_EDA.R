90864011d719d6b2d9f7bd0302fb0a92caaac942
Busra Yilmazturk <busraa.tull@gmail.com>
2024-04-06 21:56:42 +0000

    csv read as df

--- hafta_6_20240404_EDA.R
+++ hafta_6_20240404_EDA.R
@@ -1,2 +1,9 @@
-library(MASS)
-set.seed(999)
+# nancy howell tarafindan toplanmis populasyon veri seti
+# boy, kilo, yas ve cinsiyet kolonlarini icerir, toplam 544 satir
+# kaynak: https://tspace.library.utoronto.ca/handle/1807/17973
+
+df = read.csv("data/Howell1.csv", sep=";")
+View(df)
+
+#library(MASS)
+#set.seed(999)
