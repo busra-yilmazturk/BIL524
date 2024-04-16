4f7a72033cc9915c541dc59e4baf9d38bb8eee49
Busra Yilmazturk <busraa.tull@gmail.com>
2024-04-16 21:00:37 +0000

    satır sutun sayilari kontrol

--- hafta_6_20240404_EDA.R
+++ hafta_6_20240404_EDA.R
@@ -2,8 +2,15 @@
 # boy, kilo, yas ve cinsiyet kolonlarini icerir, toplam 544 satir
 # kaynak: https://tspace.library.utoronto.ca/handle/1807/17973
 
+# veriyi oku
 df = read.csv("data/Howell1.csv", sep=";")
-View(df)
 
-#library(MASS)
-#set.seed(999)
+# veriyi kontrol et
+head(df)
+
+# satir-kolon sayilarini kontrol et
+paste("Satır Sayısı: ", nrow(df), sep=" ") # 544
+paste("Kolon Sayısı: ", ncol(df), sep=" ") # 4
+
+# veriyi ozetlestirerek kolon bazinda temel istatistiklere bak
+summary(df)
