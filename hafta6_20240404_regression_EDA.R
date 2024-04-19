0645908af60cc0ab2f4ec0935ec1ea1f2868e3b4
Busra Yilmazturk <busraa.tull@gmail.com>
2024-04-19 21:28:56 +0000

    sentetik veri uretimi

--- hafta6_20240404_regression_EDA.R
+++ hafta6_20240404_regression_EDA.R
@@ -1,3 +1,6 @@
+#
+# HAZIR VERI SETINDEN KESIFSEL VERI ANALIZI
+# 
 # Brett Lantz tarafindan yazilmis "Machine Learning with R" kitabindan alinmis
 # saglik sigorta giderleri veri seti
 # 
@@ -95,3 +98,83 @@ corrplot(corr_mat, method="number")
 
 # scatterplot/sacilim grafigi olusturma (ikili ciftler halinde kontrol)
 plot(df_all[c("age", "bmi", "children", "charges")])
+
+#
+# VERI URETEREK KESIFSEL VERI ANALIZI
+#
+
+library(MASS)
+
+# tekrarlanabilirlik için seed belirleme
+set.seed(999) 
+
+# sample sayisi
+n <- 100 
+
+# bagimsiz degiskenler
+x1 <- rnorm(n, mean = 10, sd = 2)
+x2 <- rnorm(n, mean = 5, sd = 1) 
+x3 <- rnorm(n, mean = 3, sd = 0.5) 
+x4 <- rnorm(n, mean = 7, sd = 1.5) 
+x5 <- rnorm(n, mean = 15, sd = 3)
+
+# bagimli degisken
+y <- 3 + 2*x1 - 1.5*x2 + 0.5*x3 + 1.8*x4 - 1.2*x5 + rnorm(n, mean = 0, sd = 2)
+
+# dataframe'e donustur
+df_sentetik <- data.frame(x1, x2, x3, x4, x5, y)
+
+# veriyi kontrol et
+head(df_sentetik)
+
+# satir-kolon sayilarini kontrol et
+paste("Satır Sayısı:", nrow(df_sentetik), sep=" ") # 100
+paste("Kolon Sayısı:", ncol(df_sentetik), sep=" ") # 6
+
+# coklamis satirlari veriden cikar
+df_sentetik_temiz <- df_sentetik[!duplicated(df_sentetik), ]
+df_sentetik_temiz
+
+# veriyi ozetlestirerek kolon bazinda temel istatistiklere bak
+summary(df_sentetik_temiz)
+
+# her bir kolon icin veri frekansina/dagilimlarina bak, histogram uretimi
+library(ggplot2)
+ggplot(df_sentetik_temiz, aes(x=x1)) + geom_histogram() + ggtitle("x1 Verisi Dağılımı")
+ggplot(df_sentetik_temiz, aes(x=x2)) + geom_histogram() + ggtitle("x2 Verisi Dağılımı")
+ggplot(df_sentetik_temiz, aes(x=x3)) + geom_histogram() + ggtitle("x3 Verisi Dağılımı")
+ggplot(df_sentetik_temiz, aes(x=x4)) + geom_histogram() + ggtitle("x4 Verisi Dağılımı")
+ggplot(df_sentetik_temiz, aes(x=x5)) + geom_histogram() + ggtitle("x5 Verisi Dağılımı")
+ggplot(df_sentetik_temiz, aes(x=y)) + geom_histogram() + ggtitle("y Verisi Dağılımı")
+
+# outlier/aykiri deger tespiti
+boxplot(df_sentetik_temiz)
+
+# "y" bazinda alt/lower ve ust/upper ceyreklikleri/quartile hesaplama
+Q1 <- quantile(df_sentetik_temiz$y, 0.25)
+Q3 <- quantile(df_sentetik_temiz$y, 0.75)
+cat("y icin -> ", "alt ceyreklik: ", Q1, "ust ceyreklik: ", Q3)
+
+# "y" bazinda alt ve ust sinir hesaplama
+IQR <- Q3 - Q1
+lower_bound <- Q1 - 1.5 * IQR
+upper_bound <- Q3 + 1.5 * IQR
+cat("y icin -> ", "alt sinir: ", lower_bound, "ust sinir: ", upper_bound)
+
+# "y" bazinda aykiri deger iceren satirlari belirleme
+outliers <- df_sentetik_temiz$y < lower_bound | df_sentetik_temiz$y > upper_bound
+df_sentetik_temiz$aykiri_deger_mi <- outliers
+df_sentetik_aykiri_degerler <- df_sentetik_temiz[df_sentetik_temiz$aykiri_deger_mi == TRUE, ]
+paste("Toplam Aykırı Değer İçeren Satır Sayısı:", nrow(df_sentetik_aykiri_degerler), sep=" ")
+
+# "y" bazinda aykiri degerlerden arindirilmis satirlari getir
+df_sentetik_hazir <- df_sentetik_temiz[df_sentetik_temiz$aykiri_deger_mi == FALSE, ]
+df_sentetik_hazir
+
+# korelasyon matrisi
+library(corrplot)
+corr_mat <- cor(df_sentetik_hazir[c("x1", "x2", "x3", "x4", "x5", "y")])
+corrplot(corr_mat, method="number")
+
+# scatterplot/sacilim grafigi olusturma (ikili ciftler halinde kontrol)
+plot(df_sentetik_hazir[c("x1", "x2", "x3", "x4", "x5", "y")])
